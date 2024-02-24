521bbc9f6825ea1f707c35630782b4dcc7e67877
Fahad <fahad04598@gmal.com>
2024-02-25 01:36:31 +0530

    update header z index

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,7 @@ import Navbar from "./nav";
 const Index = () => {
   return (
     <>
-      <div className="z-[10 fixed inset-0 h-[80px] border-b-[2px] border-[#999999] border-opacity-20 bg-white xl:h-[88px] ">
+      <div className="z-[10] fixed inset-0 h-[80px] border-b-[2px] border-[#999999] border-opacity-20 bg-white xl:h-[88px] ">
         <div className=" relative mx-auto flex h-full w-[88%] max-w-[1200px] items-center justify-between xl:justify-start xl:gap-x-[35px]">
           <div className="relative h-[48px] w-[120px]">
             <Image alt="logo" fill src="/assets/images/logo.svg" />
